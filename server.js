d866890f17ab32f38eb6596eba2dc00cc20c139e
alexandrazykova <alexandrazykova1@gmail.com>
2023-09-10 21:51:22 -0400

    added GET route for notes, imported fs, added procrss.env.port to server.js

--- server.js
+++ server.js
@@ -3,22 +3,25 @@ const express = require('express');
 // Import built-in Node.js package 'path'
 const path = require('path');
 
-const PORT = 3001;
+const notes = require('./db/db.json');
+const fs = require('fs');
+
+const PORT = process.env.port || 3001;
 const app = express();
 
-// Parse incoming string or array data
-app.use(express.urlencoded({ extended: true}));
-// Parse incoming JSON data
 app.use(express.json());
-app.use(express.static('public'));
 
+app.use(express.static('public'));
 
 // GET Route for home page
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
